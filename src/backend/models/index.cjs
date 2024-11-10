c6315c5152b39b694e491449a4c4bb5a2b440cf9
NannisNF <92763415+NannisNF@users.noreply.github.com>
2024-11-09 18:27:12 -0600

    index_modelos

--- src/backend/models/index.cjs
+++ src/backend/models/index.cjs
@@ -6,8 +6,10 @@ const Avatares = require("./Avatares.cjs");
 const Amistad = require("./Amistad.cjs");
 
 Publicacion.belongsTo(Usuarios, { foreignKey: "uid_post", as: "user" });
+Publicacion.hasMany(Likes, { foreignKey: "id_publicacion", as: "likes" });
 Likes.belongsTo(Usuarios, { foreignKey: "id_usuario", as: "user" });
 Likes.belongsTo(Publicacion, { foreignKey: "id_publicacion", as: "post" });
+Usuarios.hasMany(Likes, { foreignKey: "id_usuario", as: "likes" });
 
 module.exports = {
   Usuarios,
